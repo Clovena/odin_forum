eab8b822177c5a9263a0bf42d1ca80963307db07
Zachary J Ricker <109747756+Clovena@users.noreply.github.com>
2025-07-25 12:26:57 -0400

    feat: update Schema.

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_25_155716) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_25_161334) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.text "link"
+    t.text "image"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "posts", "users"
 end
