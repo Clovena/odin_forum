dd1e025e6121b5576e67330136fc942d690b6b18
Zachary J Ricker <109747756+Clovena@users.noreply.github.com>
2025-07-25 12:57:16 -0400

    feat: declare associations between comments and other models.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :posts
+  has_many :comments
 
   validates :username, presence: true, uniqueness: true, length: { minimum: 4 }
   validates :email, presence: true, uniqueness: true
